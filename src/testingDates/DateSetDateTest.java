72534663161b0dafb8b30effd568d374efec07ca
kevin-krill <kkrill2@emich.edu>
2025-07-19 18:52:47 -0400

    tests written

--- src/testingDates/DateSetDateTest.java
+++ src/testingDates/DateSetDateTest.java
@@ -6,19 +6,35 @@ import static org.junit.Assert.*;
 public class DateSetDateTest {
 
 	@Test
-	public void testingIllegalDay{
-		Date date = new 
-		assertEquals();
+	public void testingIllegalDay(){
+		Date date = new Date();
+		Date expected = new Date(7, 40, 2020);
+		
+		date.setDate(7, 40, 2020);
+		assertEquals(expected,date);
 	}
 	
 	@Test
-	public void testingIllegalMonth{
+	public void testingIllegalMonth(){
+		//int
+		Date date = new Date(1,1,2020);
 		
+		date.setDate(-1, 1, 2020);
+		assertEquals(1,date.getMonth());
+		
+		//string
+		Date date2 = new Date(1,1,2020);
+		
+		date2.setDate("Apple", 1, 2020);
+		assertEquals(1,date2.getMonth());
 	}
 	
 	@Test
-	public void testingIllegalYear{
+	public void testingIllegalYear(){
+		Date date = new Date(1,1,2020);
 		
+		date.setDate(1,1,-1);
+		assertEquals(1,1,2020);
 	}
 	
 }
