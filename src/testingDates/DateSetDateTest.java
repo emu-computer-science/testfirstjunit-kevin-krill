7653a527f75d41fa4b77d79049553c629842e572
kevin-krill <kkrill2@emich.edu>
2025-07-19 13:27:12 -0400

    saved progress

---
+++ src/testingDates/DateSetDateTest.java
@@ -0,0 +1,24 @@
+package testingDates;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DateSetDateTest {
+
+	@Test
+	public void testingIllegalDay{
+		Date date = new 
+		assertEquals();
+	}
+	
+	@Test
+	public void testingIllegalMonth{
+		
+	}
+	
+	@Test
+	public void testingIllegalYear{
+		
+	}
+	
+}
