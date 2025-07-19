fc89df4b1e345c8de7f7a16b8a3162671d498372
kevin-krill <kkrill2@emich.edu>
2025-07-19 13:23:09 -0400

    finished DateAddDaysTest

--- src/testingDates/DateAddDaysTest.java
+++ src/testingDates/DateAddDaysTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 public class DateAddDaysTest {
 
+	//add more cases
 	@Test
 	public void testSameMonth() {
 		
@@ -13,6 +14,25 @@ public class DateAddDaysTest {
 		
 		assertEquals(expected, date.addOneDay());
 		
+		Date date2 = new Date(3, 1, 2020);
+		Date expected2 = new Date(3, 2, 202);
+		
+		assertEquals(expected2, date2.addOneDay());
 	}
 	
+	@Test
+	public void crossingMonthBoundary() {
+		Date date = new Date("July",31,2000);
+		Date expected = new Date("August",1,2000);
+		
+		assertEquals(expected, date.addOneDay());
+	}
+	
+	@Test
+	public void crossingYearBoundary() {
+			Date date = new Date("December",31,1999);
+			Date expected = new Date("January",1,2000);
+			
+			assertEquals(expected,date.addOneDay());
+	}
 }
